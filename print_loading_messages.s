1062658921e03f8b40aba2303684fc96fbbf3f6e
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2022-04-03 13:00:11 -0700

    Change opcodes to global reference

--- print_loading_messages.s
+++ print_loading_messages.s
@@ -3,11 +3,11 @@
 patch=0,EE,20592D70,extended,27BDFFE0   //addiu   $sp, -0x20
 patch=0,EE,20592D74,extended,FFBF0010   //sd      $ra, 0x20+var_10($sp)
 patch=0,EE,20592D78,extended,7FB00000   //sq      $s0, 0x20+var_20($sp)
-patch=0,EE,20592D7C,extended,00A02021   //move    $a0, $a1 #Get the second loading bar msg from stack
+patch=0,EE,20592D7C,extended,00A02021   //addu    $a0, $a1, $zero  #Get the second loading bar msg from stack
 patch=0,EE,20592D80,extended,0C153DF4   //jal     _printf
 patch=0,EE,20592D84,extended,00000000   //nop
 patch=0,EE,20592D88,extended,3C040059   //lui     $a0, 0x5A
-patch=0,EE,20592D8C,extended,24842DAC   //li      $a0, endl
+patch=0,EE,20592D8C,extended,24842DAC   //addiu   $a0, endl
 patch=0,EE,20592D90,extended,0C153DF4   //jal     _printf
 patch=0,EE,20592D94,extended,00000000   //nop
 patch=0,EE,20592D98,extended,DFBF0010   //ld      $ra, 0x20+var_10($sp)
